9926db71e00f6e1b92efcd95254e11c044f1fb9a
DESKTOP-9GVJBJP\Reishi <sebastian.kubrycz@gmail.com>
2024-09-06 17:27:38 +0200

    feature: WIP Initial GameMain layout

--- client/components/game_components/GameMain/GameMain.tsx
+++ client/components/game_components/GameMain/GameMain.tsx
@@ -3,7 +3,11 @@ import "./GameMain.scss";
 export default function GameMain() {
   return (
     <main className="game-main">
-      <div>GameMain content</div>
+      <div className="main-current-level">Level 14</div>
+      <div className="main-current-stage">3/10</div>
+      <div className="main-planet-name">Planet name</div>
+      <div className="main-planet-image"></div>
+      {/* Temporary */}
     </main>
   );
 }
